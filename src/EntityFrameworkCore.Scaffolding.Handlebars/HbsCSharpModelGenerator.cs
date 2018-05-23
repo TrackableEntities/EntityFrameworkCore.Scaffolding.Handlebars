03bcc15fbfa8d8c0c8c50148361ca6236dcb4c45
Anthony Sneed <tony@tonysneed.com>
2018-05-23 15:00:41 -0500

    Implement HbsCSharpModelGenerator.

--- src/EntityFrameworkCore.Scaffolding.Handlebars/HbsCSharpModelGenerator.cs
+++ src/EntityFrameworkCore.Scaffolding.Handlebars/HbsCSharpModelGenerator.cs
@@ -1,5 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore.Metadata;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+// Modifications copyright(C) 2018 Tony Sneed.
+
+using System.IO;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.EntityFrameworkCore.Scaffolding;
+using Microsoft.EntityFrameworkCore.Scaffolding.Internal;
 
 namespace EntityFrameworkCore.Scaffolding.Handlebars
 {
@@ -8,12 +16,30 @@ namespace EntityFrameworkCore.Scaffolding.Handlebars
     /// </summary>
     public class HbsCSharpModelGenerator : ModelCodeGenerator
     {
+        private const string FileExtension = ".cs";
+
+        /// <summary>
+        /// DbContext generator.
+        /// </summary>
+        public virtual ICSharpDbContextGenerator CSharpDbContextGenerator { get; }
+
+        /// <summary>
+        /// Entity type generator.
+        /// </summary>
+        public virtual ICSharpEntityTypeGenerator CSharpEntityTypeGenerator { get; }
+
         /// <summary>
         /// Constructor for the HbsCSharpModelGenerator.
         /// </summary>
         /// <param name="dependencies">Service dependencies parameter class for HbsCSharpModelGenerator.</param>
-        public HbsCSharpModelGenerator(ModelCodeGeneratorDependencies dependencies) : base(dependencies)
+        /// <param name="cSharpDbContextGenerator">DbContext generator.</param>
+        /// <param name="cSharpEntityTypeGenerator">Entity type generator.</param>
+        public HbsCSharpModelGenerator(ModelCodeGeneratorDependencies dependencies,
+            ICSharpDbContextGenerator cSharpDbContextGenerator,
+            ICSharpEntityTypeGenerator cSharpEntityTypeGenerator) : base(dependencies)
         {
+            CSharpDbContextGenerator = cSharpDbContextGenerator;
+            CSharpEntityTypeGenerator = cSharpEntityTypeGenerator;
         }
 
         /// <summary>Generates code for a model.</summary>
@@ -24,15 +50,34 @@ namespace EntityFrameworkCore.Scaffolding.Handlebars
         /// <param name="connectionString"> The connection string. </param>
         /// <param name="options"> The options to use during generation. </param>
         /// <returns> The generated model. </returns>
-        public override ScaffoldedModel GenerateModel(IModel model, string @namespace, string contextDir, string contextName,
-            string connectionString, ModelCodeGenerationOptions options)
+        public override ScaffoldedModel GenerateModel(IModel model,
+            string @namespace,
+            string contextDir,
+            string contextName,
+            string connectionString,
+            ModelCodeGenerationOptions options)
         {
-            throw new System.NotImplementedException();
+            var resultingFiles = new ScaffoldedModel();
+
+            var generatedCode = CSharpDbContextGenerator.WriteCode(model, @namespace, contextName, connectionString, options.UseDataAnnotations, options.SuppressConnectionStringWarning);
+
+            var dbContextFileName = contextName + FileExtension;
+            resultingFiles.ContextFile = new ScaffoldedFile { Path = Path.Combine(contextDir, dbContextFileName), Code = generatedCode };
+
+            foreach (var entityType in model.GetEntityTypes())
+            {
+                generatedCode = CSharpEntityTypeGenerator.WriteCode(entityType, @namespace, options.UseDataAnnotations);
+
+                var entityTypeFileName = entityType.DisplayName() + FileExtension;
+                resultingFiles.AdditionalFiles.Add(new ScaffoldedFile { Path = entityTypeFileName, Code = generatedCode });
+            }
+
+            return resultingFiles;
         }
 
         /// <summary>
         /// Gets the programming language supported by this service.
         /// </summary>
-        public override string Language { get; }
+        public override string Language => "C#";
     }
 }

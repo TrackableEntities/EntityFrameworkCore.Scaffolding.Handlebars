53d05f2199a8707e74f951bd917237581759c6f0
Olivier Matrot <olivier.matrot@rte.fr>
2019-07-12 15:35:24 +0200

    Updated the sample

--- sample/ScaffoldingSample/Sample.ReadMe.md
+++ sample/ScaffoldingSample/Sample.ReadMe.md
@@ -31,11 +31,14 @@ public class ScaffoldingDesignTimeServices : IDesignTimeServices
         // Register Handlebars helper
         var myHelper = (helperName: "my-helper", helperFunction: (Action<TextWriter, Dictionary<string, object>, object[]>) MyHbsHelper);
 
+        var ifCondHelper = (helperName: "ifCond", helperFunction: (Action<TextWriter, HelperOptions, Dictionary<string, object>, object[]>)MyHbsBlockHelper);
+
         // Add Handlebars scaffolding templates
         services.AddHandlebarsScaffolding(options);
 
         // Add optional Handlebars helpers
         services.AddHandlebarsHelpers(myHelper);
+        services.AddHandlebarsBlockHelpers(ifCondHelper);
 
         // Add Handlebars transformer for Country property
         services.AddHandlebarsTransformers(
@@ -57,6 +60,132 @@ public class ScaffoldingDesignTimeServices : IDesignTimeServices
     void MyHbsHelper(TextWriter writer, Dictionary<string, object> context, object[] parameters)
     {
         writer.Write("// My Handlebars Helper");
+    }
+
+    void MyHbsBlockHelper(TextWriter writer, HelperOptions options, Dictionary<string, object> context, object[] args)
+    {
+        if (args.Length != 3)
+        {
+            writer.Write("ifCond:Wrong number of arguments");
+            return;
+        }
+        if (args[0] == null || args[0].GetType().Name == "UndefinedBindingResult")
+        {
+            writer.Write("ifCond:args[0] undefined");
+            return;
+        }
+        if (args[1] == null || args[1].GetType().Name == "UndefinedBindingResult")
+        {
+            writer.Write("ifCond:args[1] undefined");
+            return;
+        }
+        if (args[2] == null || args[2].GetType().Name == "UndefinedBindingResult")
+        {
+            writer.Write("ifCond:args[2] undefined");
+            return;
+        }
+        if (args[0].GetType().Name == "String")
+        {
+            var val1 = args[0].ToString();
+            var val2 = args[2].ToString();
+
+            switch (args[1].ToString())
+            {
+                case ">":
+                    if (val1.Length > val2.Length)
+                    {
+                        options.Template(writer, (object)context);
+                    }
+                    else
+                    {
+                        options.Inverse(writer, (object)context);
+                    }
+                    break;
+                case "=":
+                case "==":
+                    if (val1 == val2)
+                    {
+                        options.Template(writer, (object)context);
+                    }
+                    else
+                    {
+                        options.Inverse(writer, (object)context);
+                    }
+                    break;
+                case "<":
+                    if (val1.Length < val2.Length)
+                    {
+                        options.Template(writer, (object)context);
+                    }
+                    else
+                    {
+                        options.Inverse(writer, (object)context);
+                    }
+                    break;
+                case "!=":
+                case "<>":
+                    if (val1 != val2)
+                    {
+                        options.Template(writer, (object)context);
+                    }
+                    else
+                    {
+                        options.Inverse(writer, (object)context);
+                    }
+                    break;
+            }
+        }
+        else
+        {
+            var val1 = float.Parse(args[0].ToString());
+            var val2 = float.Parse(args[2].ToString());
+
+            switch (args[1].ToString())
+            {
+                case ">":
+                    if (val1 > val2)
+                    {
+                        options.Template(writer, (object)context);
+                    }
+                    else
+                    {
+                        options.Inverse(writer, (object)context);
+                    }
+                    break;
+                case "=":
+                case "==":
+                    if (val1 == val2)
+                    {
+                        options.Template(writer, (object)context);
+                    }
+                    else
+                    {
+                        options.Inverse(writer, (object)context);
+                    }
+                    break;
+                case "<":
+                    if (val1 < val2)
+                    {
+                        options.Template(writer, (object)context);
+                    }
+                    else
+                    {
+                        options.Inverse(writer, (object)context);
+                    }
+                    break;
+                case "!=":
+                case "<>":
+                    if (val1 != val2)
+                    {
+                        options.Template(writer, (object)context);
+                    }
+                    else
+                    {
+                        options.Inverse(writer, (object)context);
+                    }
+                    break;
+            }
+        }
     }
 }
 ```
--- sample/ScaffoldingSample/ScaffoldingDesignTimeServices.cs
+++ sample/ScaffoldingSample/ScaffoldingDesignTimeServices.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using EntityFrameworkCore.Scaffolding.Handlebars;
+using HandlebarsDotNet;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -25,11 +26,15 @@ namespace ScaffoldingSample
             // Register Handlebars helper
             var myHelper = (helperName: "my-helper", helperFunction: (Action<TextWriter, Dictionary<string, object>, object[]>) MyHbsHelper);
 
+            var ifCondHelper = (helperName: "ifCond", helperFunction: (Action<TextWriter, HelperOptions, Dictionary<string, object>, object[]>)MyHbsBlockHelper);
+
+
             // Add Handlebars scaffolding templates
             services.AddHandlebarsScaffolding(options);
 
             // Add optional Handlebars helpers
             services.AddHandlebarsHelpers(myHelper);
+            services.AddHandlebarsBlockHelpers(ifCondHelper);
 
             // Add Handlebars transformer for Country property
             services.AddHandlebarsTransformers(
@@ -51,6 +56,133 @@ namespace ScaffoldingSample
         void MyHbsHelper(TextWriter writer, Dictionary<string, object> context, object[] parameters)
         {
             writer.Write("// My Handlebars Helper");
+        }
+
+        // Sample Handlebars block helper
+        void MyHbsBlockHelper(TextWriter writer, HelperOptions options, Dictionary<string, object> context, object[] args)
+        {
+            if (args.Length != 3)
+            {
+                writer.Write("ifCond:Wrong number of arguments");
+                return;
+            }
+            if (args[0] == null || args[0].GetType().Name == "UndefinedBindingResult")
+            {
+                writer.Write("ifCond:args[0] undefined");
+                return;
+            }
+            if (args[1] == null || args[1].GetType().Name == "UndefinedBindingResult")
+            {
+                writer.Write("ifCond:args[1] undefined");
+                return;
+            }
+            if (args[2] == null || args[2].GetType().Name == "UndefinedBindingResult")
+            {
+                writer.Write("ifCond:args[2] undefined");
+                return;
+            }
+            if (args[0].GetType().Name == "String")
+            {
+                var val1 = args[0].ToString();
+                var val2 = args[2].ToString();
+
+                switch (args[1].ToString())
+                {
+                    case ">":
+                        if (val1.Length > val2.Length)
+                        {
+                            options.Template(writer, (object)context);
+                        }
+                        else
+                        {
+                            options.Inverse(writer, (object)context);
+                        }
+                        break;
+                    case "=":
+                    case "==":
+                        if (val1 == val2)
+                        {
+                            options.Template(writer, (object)context);
+                        }
+                        else
+                        {
+                            options.Inverse(writer, (object)context);
+                        }
+                        break;
+                    case "<":
+                        if (val1.Length < val2.Length)
+                        {
+                            options.Template(writer, (object)context);
+                        }
+                        else
+                        {
+                            options.Inverse(writer, (object)context);
+                        }
+                        break;
+                    case "!=":
+                    case "<>":
+                        if (val1 != val2)
+                        {
+                            options.Template(writer, (object)context);
+                        }
+                        else
+                        {
+                            options.Inverse(writer, (object)context);
+                        }
+                        break;
+                }
+            }
+            else
+            {
+                var val1 = float.Parse(args[0].ToString());
+                var val2 = float.Parse(args[2].ToString());
+
+                switch (args[1].ToString())
+                {
+                    case ">":
+                        if (val1 > val2)
+                        {
+                            options.Template(writer, (object)context);
+                        }
+                        else
+                        {
+                            options.Inverse(writer, (object)context);
+                        }
+                        break;
+                    case "=":
+                    case "==":
+                        if (val1 == val2)
+                        {
+                            options.Template(writer, (object)context);
+                        }
+                        else
+                        {
+                            options.Inverse(writer, (object)context);
+                        }
+                        break;
+                    case "<":
+                        if (val1 < val2)
+                        {
+                            options.Template(writer, (object)context);
+                        }
+                        else
+                        {
+                            options.Inverse(writer, (object)context);
+                        }
+                        break;
+                    case "!=":
+                    case "<>":
+                        if (val1 != val2)
+                        {
+                            options.Template(writer, (object)context);
+                        }
+                        else
+                        {
+                            options.Inverse(writer, (object)context);
+                        }
+                        break;
+                }
+            }
         }
     }
 }

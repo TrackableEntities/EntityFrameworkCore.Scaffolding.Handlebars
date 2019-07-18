2473889e9eb99a424362b119a7f456ee80659776
Olivier Matrot <olivier.matrot@rte.fr>
2019-07-18 09:37:04 +0200

    Revised the MyHbsBlockHelper

--- sample/ScaffoldingSample/ScaffoldingDesignTimeServices.cs
+++ sample/ScaffoldingSample/ScaffoldingDesignTimeServices.cs
@@ -28,7 +28,6 @@ namespace ScaffoldingSample
 
             var ifCondHelper = (helperName: "ifCond", helperFunction: (Action<TextWriter, HelperOptions, Dictionary<string, object>, object[]>)MyHbsBlockHelper);
 
-
             // Add Handlebars scaffolding templates
             services.AddHandlebarsScaffolding(options);
 
@@ -61,127 +60,37 @@ namespace ScaffoldingSample
         // Sample Handlebars block helper
         void MyHbsBlockHelper(TextWriter writer, HelperOptions options, Dictionary<string, object> context, object[] args)
         {
-            if (args.Length != 3)
-            {
-                writer.Write("ifCond:Wrong number of arguments");
-                return;
-            }
-            if (args[0] == null || args[0].GetType().Name == "UndefinedBindingResult")
-            {
-                writer.Write("ifCond:args[0] undefined");
-                return;
-            }
-            if (args[1] == null || args[1].GetType().Name == "UndefinedBindingResult")
-            {
-                writer.Write("ifCond:args[1] undefined");
-                return;
-            }
-            if (args[2] == null || args[2].GetType().Name == "UndefinedBindingResult")
-            {
-                writer.Write("ifCond:args[2] undefined");
-                return;
-            }
-            if (args[0].GetType().Name == "String")
-            {
-                var val1 = args[0].ToString();
-                var val2 = args[2].ToString();
+            var val1 = float.Parse(args[0].ToString());
+            var val2 = float.Parse(args[2].ToString());
 
-                switch (args[1].ToString())
-                {
-                    case ">":
-                        if (val1.Length > val2.Length)
-                        {
-                            options.Template(writer, context);
-                        }
-                        else
-                        {
-                            options.Inverse(writer, context);
-                        }
-                        break;
-                    case "=":
-                    case "==":
-                        if (val1 == val2)
-                        {
-                            options.Template(writer, context);
-                        }
-                        else
-                        {
-                            options.Inverse(writer, context);
-                        }
-                        break;
-                    case "<":
-                        if (val1.Length < val2.Length)
-                        {
-                            options.Template(writer, context);
-                        }
-                        else
-                        {
-                            options.Inverse(writer, context);
-                        }
-                        break;
-                    case "!=":
-                    case "<>":
-                        if (val1 != val2)
-                        {
-                            options.Template(writer, context);
-                        }
-                        else
-                        {
-                            options.Inverse(writer, context);
-                        }
-                        break;
-                }
-            }
-            else
+            switch (args[1].ToString())
             {
-                var val1 = float.Parse(args[0].ToString());
-                var val2 = float.Parse(args[2].ToString());
-
-                switch (args[1].ToString())
-                {
-                    case ">":
-                        if (val1 > val2)
-                        {
-                            options.Template(writer, context);
-                        }
-                        else
-                        {
-                            options.Inverse(writer, context);
-                        }
-                        break;
-                    case "=":
-                    case "==":
-                        if (val1 == val2)
-                        {
-                            options.Template(writer, context);
-                        }
-                        else
-                        {
-                            options.Inverse(writer, context);
-                        }
-                        break;
-                    case "<":
-                        if (val1 < val2)
-                        {
-                            options.Template(writer, context);
-                        }
-                        else
-                        {
-                            options.Inverse(writer, context);
-                        }
-                        break;
-                    case "!=":
-                    case "<>":
-                        if (val1 != val2)
-                        {
-                            options.Template(writer, context);
-                        }
-                        else
-                        {
-                            options.Inverse(writer, context);
-                        }
-                        break;
-                }
+                case ">":
+                    if (val1 > val2)
+                        options.Template(writer, context);
+                    else
+                        options.Inverse(writer, context);
+                    break;
+                case "=":
+                case "==":
+                    if (val1 == val2)
+                        options.Template(writer, context);
+                    else
+                        options.Inverse(writer, context);
+                    break;
+                case "<":
+                    if (val1 < val2)
+                        options.Template(writer, context);
+                    else
+                        options.Inverse(writer, context);
+                    break;
+                case "!=":
+                case "<>":
+                    if (val1 != val2)
+                        options.Template(writer, context);
+                    else
+                        options.Inverse(writer, context);
+                    break;
             }
         }
     }

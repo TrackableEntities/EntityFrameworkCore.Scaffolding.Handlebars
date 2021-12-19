31f5ac734b1b296fee8884f8a3fa6cd99d52f155
Anthony Sneed <tony.sneed@icloud.com>
2021-12-19 16:39:29 -0600

    Update Handlebars.Net to fix issue with character encoding.

--- test/Scaffolding.Handlebars.Tests/ExpectedEntities.cs
+++ test/Scaffolding.Handlebars.Tests/ExpectedEntities.cs
@@ -104,6 +104,60 @@ namespace FakeNamespace
 ";
         }
 
+        private static class ExpectedEntitiesNoEncoding
+        {
+            public const string CategoryClass =
+                @"using System;
+using System.Collections.Generic;
+
+namespace FakeNamespace
+{
+    /// <summary>
+    /// 产品
+    /// </summary>
+    public partial class Category : Entity<int>
+    {
+        public Category()
+        {
+            Products = new HashSet<Product>();
+        }
+
+        public int CategoryId { get; set; }
+
+        /// <summary>
+        /// The name of a category
+        /// </summary>
+        public string CategoryName { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
+";
+
+            public const string ProductClass =
+                @"using System;
+using System.Collections.Generic;
+
+namespace FakeNamespace
+{
+    /// <summary>
+    /// 产品
+    /// </summary>
+    public partial class Product : Entity<int>
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal? UnitPrice { get; set; }
+        public bool Discontinued { get; set; }
+        public byte[] RowVersion { get; set; }
+        public int? CategoryId { get; set; }
+
+        public virtual Category Category { get; set; }
+    }
+}
+";
+        }
+
         private static class ExpectedEntitiesWithAnnotations
         {
             public const string CategoryClass =
@@ -243,6 +297,79 @@ namespace FakeNamespace
         [ForeignKey(nameof(CategoryIdRenamed))]
         [InverseProperty(""Products"")]
         public virtual CategoryRenamed Category { get; set; }
+    }
+}
+";
+        }
+
+        private static class ExpectedEntitiesWithAnnotationsNoEncoding
+        {
+            public const string CategoryClass =
+                @"using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace FakeNamespace
+{
+    /// <summary>
+    /// 产品
+    /// </summary>
+    [Table(""Category"")]
+    public partial class Category : Entity<int>
+    {
+        public Category()
+        {
+            Products = new HashSet<Product>();
+        }
+
+        [Key]
+        public int CategoryId { get; set; }
+
+        /// <summary>
+        /// The name of a category
+        /// </summary>
+        [Required]
+        [StringLength(15)]
+        public string CategoryName { get; set; }
+
+        [InverseProperty(nameof(Product.Category))]
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
+";
+
+            public const string ProductClass =
+                @"using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace FakeNamespace
+{
+    /// <summary>
+    /// 产品
+    /// </summary>
+    [Table(""Product"")]
+    [Index(nameof(CategoryId), Name = ""IX_Product_CategoryId"")]
+    public partial class Product : Entity<int>
+    {
+        [Key]
+        public int ProductId { get; set; }
+        [Required]
+        [StringLength(40)]
+        public string ProductName { get; set; }
+        [Column(TypeName = ""money"")]
+        public decimal? UnitPrice { get; set; }
+        public bool Discontinued { get; set; }
+        public byte[] RowVersion { get; set; }
+        public int? CategoryId { get; set; }
+
+        [ForeignKey(nameof(CategoryId))]
+        [InverseProperty(""Products"")]
+        public virtual Category Category { get; set; }
     }
 }
 ";

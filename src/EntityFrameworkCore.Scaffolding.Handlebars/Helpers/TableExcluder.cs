60f4fd6a938068db4b986dc2256258e254c1b9d3
Randell Hodges <randell.hodges@trwd.com>
2022-06-01 09:29:26 -0500

    Simple glob/wildcard matching of table names

---
+++ src/EntityFrameworkCore.Scaffolding.Handlebars/Helpers/TableExcluder.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace EntityFrameworkCore.Scaffolding.Handlebars.Helpers
+{
+    internal static class TableExcluder
+    {
+        public static bool IsExcluded(HandlebarsScaffoldingOptions options, IEntityType type)
+        {
+            if (options?.ExcludedTables?.Any() == true)
+            {
+                var schema = type.GetSchema() ?? type.GetViewSchema();
+                var table = type.GetTableName() ?? type.GetViewName();
+
+                var name = $"{schema}.{table}";
+
+                foreach (var exclusion in options?.ExcludedTables)
+                {
+                    // For backwards compatibility, if there is no ".", then we'll assume it is a table/view to be exluded
+                    // and add the schema wildcard
+                    var pattern = exclusion.Contains('.')
+                        ? exclusion
+                        : $"*.{exclusion}";
+
+                    // Simple glob emulation, credits to:
+                    // https://stackoverflow.com/questions/188892/glob-pattern-matching-in-net
+                    pattern = "^" + Regex.Escape(pattern).Replace(@"\*", ".*").Replace(@"\?", ".") + "$";
+                    if (Regex.IsMatch(name, pattern, RegexOptions.IgnoreCase | RegexOptions.Singleline))
+                        return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
